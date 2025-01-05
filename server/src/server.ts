9bf0b7f7c46fcdf4cb8a2dc46f283ac4e3861f4d
Alok choudhary <alokc8502@gmail.com>
2025-01-05 14:36:33 +0530

    connect to DB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,29 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './db/connectDB';
+
+
+dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 3001; // Port for the server
-  
 app.use(cors()); // Enable CORS if needed
 app.use(express.json()); // Parse incoming JSON requests
 
 // Define API routes here
-app.get('/api/data', (_, res) => {
-  res.json({ message: 'Hello from the backend!' });
+app.get("/", (_, res) => {
+  res.send("API Working with /api/v1");
 });
 
+//API ROUTES
+
+
+
+
 app.listen(port, () => {
+  console.log(`mogodb: ${process.env.MONGODB_URI}`);
+  console.log(`port: ${process.env.PORT}`);
+  connectDB(process.env.MONGODB_URI || '');
   console.log(`Server listening on port ${port}`);
 });
